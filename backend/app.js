bb4a78850d247508bdb0748775bb1dd5f12042a3
JFT49 <jf.thom49@gmail.com>
2021-12-05 19:39:59 +0100

    1er push

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Toto:0@cluster0.ipn9j.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+  });
+
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
